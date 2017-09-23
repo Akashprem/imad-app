fcf6e7078063713aac86d7f8fdb51ec8464829fb
Akashprem <30836242+Akashprem@users.noreply.github.com>
2017-09-23 17:00:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ function createTemplate(cont) {
         <title>
             ${title}
         </title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" type="type/css" href="style.css">
     </head>
     <body>
         <div>
