482275e41d8cf85a5d082ce672ff60a99015eac3
Akashprem <30836242+Akashprem@users.noreply.github.com>
2017-09-23 15:28:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
   articleName = req.params.articleName
-  res.send(createTemplate('article-one'));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
