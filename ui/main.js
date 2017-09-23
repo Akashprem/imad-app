ca7fee0d5ff348f92ab0e5dc3e5122dc401b2df6
Akashprem <30836242+Akashprem@users.noreply.github.com>
2017-09-23 18:35:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 counter = document.getElementById("counter");
 
 counter.onclick = function() {
-    count = document.getElementById("count").value;
+    count = parseInt(document.getElementById("count").value);
     count = count + 1;
     document.getElementById("count").innerHTML = count;
 };
